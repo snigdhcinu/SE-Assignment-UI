429fbbc2c469f0573936f4a04261f7b239656845
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-05-30 01:06:12 +0530

    to scroll non-abruptly
    script added so that on scrolling down to hyperlink, it scrolls down gradually, and elegantly

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // script file
+document.querySelectorAll('a[href^="#projects"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
